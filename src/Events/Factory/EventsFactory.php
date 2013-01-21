474dcc986ecc969e0f7f3f45141f136769199219
ptitdam2001 <dsuhard@gmail.com>
2013-01-21 14:07:23 +0100

    import projet

---
+++ src/Events/Factory/EventsFactory.php
@@ -0,0 +1,47 @@
+<?php
+namespace Events\Factory;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Factory de création du service d'evenements
+ * @author dsuhard
+ *
+ */
+class EventsFactory implements FactoryInterface
+{
+	/**
+	 * (non-PHPdoc)
+	 * @see Zend\ServiceManager.FactoryInterface::createService()
+	 */
+	public function createService(ServiceLocatorInterface $serviceLocator)
+	{
+		$eventManager = $serviceLocator->get('EventManager')->getSharedManager();
+
+		$config = $serviceLocator->get('EventsConfig');
+
+		if (!empty($config)) {
+			foreach ($config as $identifier => $events) {
+				foreach ($events as $eventName => $callbacks) {
+					foreach ($callbacks as $callback) {
+						if ($callback['service'] !== '' && $callback['function'] !== '') {
+							if ($serviceLocator->has($callback['service'])) {
+								$serv = $serviceLocator->get($callback['service']);
+
+								if (!isset($callback['priority'])) {
+									$eventManager->attach($identifier, $eventName, array($serv, $callback['function']));
+								} else {
+									$eventManager->attach($identifier, $eventName, array($serv, $callback['function']), $callback['priority']);
+								}
+							} else {
+								error_log($callback['service'] . ' not defined service');
+							}
+						}
+					}
+				}
+			}
+		}
+		return $this;
+	}
+}
